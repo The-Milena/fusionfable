39abe253f8e8795e0fb22624ba7106a0c4bacd5a
Milena Arms <183767485+The-Milena@users.noreply.github.com>
2025-09-10 16:21:43 +0200

    OPENAI access

--- config/initializers/openai.rb
+++ config/initializers/openai.rb
@@ -1,4 +1,4 @@
 OpenAI.configure do |config|
-  config.access_token = ENV.fetch("OPENAI_ACCESS_TOKEN")
+  config.access_token = ENV.fetch("OPENAI_API_KEY")
   config.log_errors = true # Enable error logging
 end
